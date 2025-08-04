0d0d68bf20559f3522bf1158f9a8c67c7e62cb60
yanx23 <meufilho229@gmail.com>
2025-08-04 09:29:29 -0300

    Criado um arquivo com exemplos de Variáveis e condicionais

---
+++ Variaveis-Condicionais.C
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    //Criando variáveis de INTEIROS
+    
+    int x = 20;
+    int y = 18;
+    
+    //Este é um condicional
+    if (x > y){
+    printf("20 é maior que 18");
+    }
+    
+    //-------------//--------------//
+    printf("\n");
+    
+    //Criando variáveis
+    int horario = 20;
+    
+    //Condicionais
+    if (horario < 18){
+        printf("Bom dia, piazada!");
+        
+    } else{
+        printf("Vai dormir, rapaz!");
+    }
+    return 0;
+}
